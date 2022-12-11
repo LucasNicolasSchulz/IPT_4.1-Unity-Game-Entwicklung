479edda30f70c6fbdb8798174f90aec23aa3bbef
Lucas.Fya <107124564+LucasNicolasSchulz@users.noreply.github.com>
2022-12-11 16:42:28 +0100

    Kommentare

--- IPT-4.1Basis5/Assets/script/OnButtonClicks.cs
+++ IPT-4.1Basis5/Assets/script/OnButtonClicks.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class OnButtonClicks : MonoBehaviour
 {
-    InputManager inputManager;
-    private void Awake()
-    {
-        inputManager = GetComponent<InputManager>();
-    }
+// Eine Variable namens "inputManager" vom Typ "InputManager" wird deklariert.
+InputManager inputManager;
+private void Awake()
+{
+// Der inputManager-Variable wird das InputManager-Komponente des GameObjects zugewiesen, dem das Skript angefügt ist.
+inputManager = GetComponent<InputManager>();
+}
 
-    private void FixedUpdate()
+
+private void FixedUpdate()
+{
+    // Wenn der "p_Input"-Wert des inputManager-Objekts "true" ist...
+    if (inputManager.p_Input)
     {
-        if (inputManager.p_Input)
-        {
-            GameObject.Find("Canvas 1").GetComponent<UI>().menu_active = !GameObject.Find("Canvas 1").GetComponent<UI>().menu_active;
-            inputManager.p_Input = false;
-        }
+        // ...wird der Wert von "menu_active" im UI-Komponente des GameObjects "Canvas 1" umgekehrt.
+        GameObject.Find("Canvas 1").GetComponent<UI>().menu_active = !GameObject.Find("Canvas 1").GetComponent<UI>().menu_active;
+        // Der "p_Input"-Wert des inputManager-Objekts wird auf "false" gesetzt.
+        inputManager.p_Input = false;
     }
+}
 }
