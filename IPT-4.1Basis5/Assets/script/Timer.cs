e9d8f7971d398607507b381f6583766d8dbccdeb
Lucas.Fya <107124564+LucasNicolasSchulz@users.noreply.github.com>
2022-12-11 16:48:38 +0100

    Kommentare

--- IPT-4.1Basis5/Assets/script/Timer.cs
+++ IPT-4.1Basis5/Assets/script/Timer.cs
@@ -1,44 +1,56 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class Timer : MonoBehaviour
 {
-  public float timeRemaining = 10;
-  public bool timerIsRunning = false;
-  public Text timeText;
-  public UI ui;
+// Zeit, die noch übrig ist
+public float timeRemaining = 10;
+// Timer läuft gerade
+public bool timerIsRunning = false;
+// Textkomponente, die die Zeit anzeigt
+public Text timeText;
+// Referenz auf UI-Skript
+public UI ui;
 
-  private void Start()
-  {
-    // Starts the timer automatically
-    timerIsRunning = true;
-    if (SceneManager.GetActiveScene().name != "IntroScene")
-    {
-      timeRemaining = PlayerPrefs.GetFloat("Timer");
-    }
-    }
-  void Update()
-  {
-    if (timerIsRunning)
-    {
-      if (timeRemaining > 0)
-      {
-        timeRemaining -= Time.deltaTime;
-        DisplayTime(timeRemaining);
-      }
-      else
-      {
-        Debug.Log("Time has run out!");
-        timeRemaining = 0;
-        timerIsRunning = false;
-      }
-    }
-  }
-  void DisplayTime(float timeToDisplay)
-  {
-    timeToDisplay += 1;
-    float minutes = Mathf.FloorToInt(timeToDisplay / 60);
-    float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-    timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-  }
+// Startet den Timer beim Start des Spiels
+private void Start()
+{
+timerIsRunning = true;
+if (SceneManager.GetActiveScene().name != "IntroScene")
+{
+// Lädt gespeicherte Zeit aus dem PlayerPrefs
+timeRemaining = PlayerPrefs.GetFloat("Timer");
+}
+}
+
+// Aktualisiert die Zeit jeden Frame
+void Update()
+{
+if (timerIsRunning)
+{
+if (timeRemaining > 0)
+{
+// Zeit wird pro Frame um deltaTime reduziert
+timeRemaining -= Time.deltaTime;
+DisplayTime(timeRemaining);
+}
+else
+{
+Debug.Log("Zeit ist abgelaufen!");
+timeRemaining = 0;
+timerIsRunning = false;
+}
+}
+}
+
+// Zeigt die Zeit in der Textkomponente an
+void DisplayTime(float timeToDisplay)
+{
+// Korrigiert die angezeigte Zeit um 1 Sekunde
+timeToDisplay += 1;
+float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+}
 }
